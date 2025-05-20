0bb33bc898a4dbb77d4d6b606d5649095146d380
Ahsan Kalam <79890238+Maky2419@users.noreply.github.com>
2025-05-20 09:20:12 -0700

    Initial Commit

---
+++ ScoreChamps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoreChamps
+//
+//  Created by Ahsan Kalam on 2025-05-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ScoreChampsTests/ScoreChampsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreChampsTests.swift
+//  ScoreChampsTests
+//
+//  Created by Ahsan Kalam on 2025-05-20.
+//
+
+import Testing
+@testable import ScoreChamps
+
+struct ScoreChampsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
