9a47bbe5bd4adc7cb9c54dd61435a7de280829da
Ahsan Kalam <79890238+Maky2419@users.noreply.github.com>
2025-06-30 19:15:02 -0700

    SignUpBranch Done

--- ScoreChamps/SignUp.swift
+++ ScoreChamps/SignUp.swift
@@ -17,16 +17,45 @@ class SignUp: UIViewController {
     @IBOutlet weak var Username: UITextField!
     @IBOutlet weak var Password: UITextField!
     
-    @IBAction func SignUpAccount(_ sender: Any) {
-        
-        
-        
-        func viewDidLoad() {
+    override func viewDidLoad() {
             super.viewDidLoad()
-          
+            ref = Database.database().reference() // Initialize Firebase reference
         }
         
+        @IBAction func SignUpAccount(_ sender: Any) {
+            guard let name = Dname.text, !name.isEmpty,
+                  let username = Username.text, !username.isEmpty,
+                  let password = Password.text, !password.isEmpty else {
+                showAlert(title: "Missing Info", message: "Please fill in all fields.")
+                return
+            }
+            
+            // Create a unique key for the new user
+            let newUserRef = ref.child("Accounts").childByAutoId()
+            
+            // Save user as dictionary (hashtable)
+            let userData: [String: Any] = [
+                "name": name,
+                "username": username,
+                "password": password
+            ]
+            
+            newUserRef.setValue(userData) { error, _ in
+                if let error = error {
+                    self.showAlert(title: "Error", message: "Failed to create account: \(error.localizedDescription)")
+                } else {
+                    self.showAlert(title: "Success", message: "Account created successfully!")
+                    // Optionally: self.dismiss(animated: true)
+                }
+            }
+        }
         
-        
-    }
+        // Reusable alert function
+        func showAlert(title: String, message: String) {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+        }
+    
+    
 }
